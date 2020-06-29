08dcd0688d0798243789f20f80b7e4afca176503
Brian Bronkhorst <brianbronkhorst@upcmail.nl>
2020-06-30 01:44:19 +0200

    user interface sjabloon aangemaakt

---
+++ public/userinterface.php
@@ -0,0 +1,113 @@
+<html>
+  <head>
+    <title>User Interface</title>
+    <link rel="stylesheet" href= "static/css/interface.css">
+  </head>
+
+  <body>
+
+    <header>
+      <div class="container-header">
+        <div class="header-menu">
+          <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Manage</a></li>
+            <li><a href="#">Markt</a></li>
+            <li><a href="#">Ranking</a></li>
+
+          </ul>
+        </div>
+        <div class="login-details">
+          <ul>
+            <li>Hello, Username123</li>
+            <li>Money: €10.000</li>
+            <li>Date: 30-6-2019</li>
+            <li><a href="#">Mijn gegevens</a></li>
+            <a href="">Logout</a>
+          <ul>
+        </div>
+      </div>
+    </div>
+    </header>
+
+    <section class="animal-tables">
+      <div class="table">
+        <h1>Dieren</h1>
+        <table>
+          <tr>
+            <th> Name  </th>
+            <th> Type </th>
+            <th> Tagname </th>
+            <th> Price </th>
+            <th> Status </th>
+          </tr>
+          <tr>
+            <td> Berta   </td>
+            <td> Cow     </td>
+            <td> Berta68 </td>
+            <td> 200,00  </td>
+            <td> Sell    </td>
+            <td><button class="primary-btn" type="button">Verkopen</button><td>
+            <td><button class="primary-btn" type="button">Voeden</button><td>
+            <td><button class="primary-btn" type="button">Slachten</button><td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <section class="product-tables">
+      <div class="table">
+        <h1>Producten</h1>
+        <table>
+          <tr>
+            <th> Name  </th>
+            <th> Type </th>
+            <th> Tagname </th>
+            <th> Price </th>
+            <th> Status </th>
+          </tr>
+          <tr>
+            <td> Berta   </td>
+            <td> Cow     </td>
+            <td> Berta68 </td>
+            <td> 200,00  </td>
+            <td> Sell    </td>
+            <td><button class="primary-btn" type="button">Verkopen</button><td>
+            <td><button class="primary-btn" type="button">Vernietigen</button><td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <section class="machine-tables">
+      <div class="table">
+        <h1>Machines</h1>
+        <table>
+          <tr>
+            <th> Name  </th>
+            <th> Type </th>
+            <th> Tagname </th>
+            <th> Price </th>
+            <th> Status </th>
+          </tr>
+          <tr>
+            <td> Berta   </td>
+            <td> Cow     </td>
+            <td> Berta68 </td>
+            <td> 200,00  </td>
+            <td> Sell    </td>
+            <td><button class="primary-btn" type="button">Verkopen</button><td>
+            <td><button class="primary-btn" type="button">Onderhoud</button><td>
+            <td><button class="primary-btn" type="button">Vernietigen</button><td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <footer>
+
+    </footer>
+  </body>
+
+
+</html>
